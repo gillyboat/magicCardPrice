d30a4df1042c52717f25f4a9221400f5110429ff
jonahw23 <jonahwitte10@gmail.com>
2023-05-04 10:47:57 -0400

    Added name parser to stat.py

--- stats.py
+++ stats.py
@@ -1,1 +1,17 @@
+import os
+import numpy as np
+from numpy import loadtxt
+from numpy import savetxt
+import json
 
+f = os.path.join("cardList")
+print(f)
+
+cardList = []
+
+with open(f, 'r') as file:
+    print("FILE", file)
+    json_data = file.read()
+    cardList = json_data
+
+print(cardList)
