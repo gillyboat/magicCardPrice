a88d17b0a980c3fc2a35cb564a895a090c48cb69
jonahw23 <jonahwitte10@gmail.com>
2023-05-08 10:02:25 -0400

    Added list of json files we care about

--- stats.py
+++ stats.py
@@ -3,6 +3,7 @@ import numpy as np
 from numpy import loadtxt
 from numpy import savetxt
 import json
+import csv
 
 f = os.path.join("cardList")
 print(f)
@@ -13,5 +14,21 @@ with open(f, 'r') as file:
     print("FILE", file)
     json_data = file.read()
     cardList = json_data
-
+    
 print(cardList)
+
+data = {}
+jsons = []
+
+import pandas as pd
+
+df = pd.read_csv("nameMap.csv")
+print(df.head())
+
+for index, row in df.iterrows():
+    if(row['name'] in cardList):
+        data[row["ID"]] = row['name']
+        jsons.append(row["ID"])
+
+print(data)
+print(jsons)
