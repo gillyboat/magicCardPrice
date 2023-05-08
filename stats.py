b7c096e3727eaff30b515593d49fd01dac43a21f
jonahw23 <jonahwitte10@gmail.com>
2023-05-08 10:59:21 -0400

    Fixed errors in data (lack of comma)

--- stats.py
+++ stats.py
@@ -14,6 +14,7 @@ with open(f, 'r') as file:
     print("FILE", file)
     json_data = file.read()
     cardList = json_data
+    cardList = cardList[2:-3].split("','")
 
 data = {}
 jsons = []
@@ -23,13 +24,28 @@ import pandas as pd
 # Parse the CSV
 
 df = pd.read_csv("nameMap.csv")
-print(df.head())
-
+checkedNames = []
 for index, row in df.iterrows():
-    if(row['name'] in cardList):
+    if(row['name'] in cardList and not(row['name'] in checkedNames)):
+        #print(row['name'])
         data[row["ID"]] = row['name']
         jsons.append(row["ID"])
+        checkedNames.append(row['name'])
+        
+#Check for errors (non-overlap)
+checkedNames.sort()
+cardList.sort()
+
+for i in range(len(cardList)):
+    if cardList[i] != checkedNames[i]:
+        print("LIST:", cardList[i] + ".", "CHECKED:", checkedNames[i] + ".")
 
 # Cardlist stores list of cards we care about
 # Data JSON stores json:cardname pairs for cards we care about
 # Jsons stores a list of all the json files we care about
+
+print("JSONS:", len(jsons))
+print("CARDLIST:", len(cardList))
+
+#print(checkedNames)
+#print(cardList)
